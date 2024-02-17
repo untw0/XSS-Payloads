33ec5c649be56b46531c0604563ee585a7e94bc9
untw <153939765+untw0@users.noreply.github.com>
2024-02-17 16:23:20 -0300

    Update xsscan.sh

--- xsscan.sh
+++ xsscan.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+# ----------------------------------------------------------------------
+# Follow Creator of Tool on Social Midia:
 # Creator of Tool: @untw0
 # Follow on Twitter: @untw0 < https://twitter.com/untw0
 # Follow on Instagram: @untw0 < https://instagram.com/untw0
+# ----------------------------------------------------------------------
+# Follow Collaborator of Tool on Social Midia:
+# Collaborator: @sput_tesla
+# Follow on Instagram: @sput_tesla < https://instagram.com/sput_tesla
+# ----------------------------------------------------------------------
 # Thx for use my Tool! It is still in the testing phase :D
+# ----------------------------------------------------------------------
 
 # Check if the number of arguments is correct
 if [ $# -ne 1 ]; then
@@ -11,14 +19,12 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
-# Check if the xss.txt file exists
 if [ ! -f "xss.txt" ]; then
     echo "File xss.txt not found."
     exit 1
 fi
 
-# URL of the website to be tested
-echo " "
+echo -e "\e[1;32m\e[0m"
 echo "======================================================================="
 echo "██╗  ██╗███████╗███████╗    ███████╗ ██████╗ █████╗ ███╗   ██╗"
 echo "╚██╗██╔╝██╔════╝██╔════╝    ██╔════╝██╔════╝██╔══██╗████╗  ██║"
@@ -28,18 +34,22 @@ echo "██╔╝ ██╗███████║███████║    
 echo "╚═╝  ╚═╝╚══════╝╚══════╝    ╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═══╝"
 echo "Follow @untw0 on all social media!"
 echo "======================================================================="
-echo " "
+echo -e "\e[1;32m\e[0m"
 url=$1
 
-# Loop through the payloads from the xss.txt file
 while IFS= read -r payload; do
     # Make the GET request with the payload injected into the URL
-    response=$(curl -s -o /dev/null -w "%{http_code}" "$url$payload")
 
-    # Check the response code and add appropriate marking
+    response=$(curl -k -s -o /dev/null -w "%{http_code}" "$url$payload")
+
+    # Check if the response code is 200 (OK)
     if [ "$response" == "200" ]; then
-        echo "Payload: $payload [ON 200]"
-    else
-        echo "Payload: $payload [ERROR 404]"
+        lengh=$(curl -k -s -o /dev/null -w "%{size_download}" "$url$payload")
+        filter=$(curl -k -s -L "$url$payload" | grep -Fsi "\$payload")
+        if $filter 2>/dev/null; then
+            echo -e "\e[1;32m[+] - $url\e[0m" 2>/dev/null
+            echo -e "\e[1;38mPayload: $payload\e[0m \e[1;32m[status: $response]\e[0m\e[1;33m[lengh: $lengh]\e[0m\n" 2>/dev/null
+        fi
     fi
+
 done < "xss.txt"
